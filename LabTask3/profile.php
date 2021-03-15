82d273e9d7d4e61bc85fd54af9b96ebf804f0a79
zihan <zihanhossain02@gmail.com>
2021-03-15 13:17:37 +0600

    Third_task:uploadfile_issu_solved

--- LabTask3/profile.php
+++ LabTask3/profile.php
@@ -1,24 +1,3 @@
-<?php
-if (isset($_POST["submit"])) {
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["image"]["name"]);
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-    $check = getimagesize($_FILES["image"]["tmp_name"]);
-    if ($check == false) {
-        $message = "File is not an image";
-    }
-    if ($_FILES["image"]["size"] > 4000000) {
-        $message = "File is too big. Max 40mb";
-    }
-    if (
-        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif"
-    ) {
-        $message = "Sorry, only JPG, JPEG, PNG files are allowed";
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,11 +11,10 @@ if (isset($_POST["submit"])) {
 <body>
     <fieldset>
         <legend>PROFILE PICTURE</legend>
-        <form method="POST" action="<?php echo $_SERVER["PHP_SELF"] ?>">
-            <input type="file" name="image" id="image">
-            <label><?php if(isset($_POST["submit"])) echo $message ?></label>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="fileToUpload" id="fileToUpload">
             <hr>
-            <input type="submit" name="submit">
+            <input type="submit" value="Upload Image" name="submit">
         </form>
     </fieldset>
 </body>
